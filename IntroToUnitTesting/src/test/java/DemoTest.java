6e97a374af63b37c39b92b32d7b3fe1cce63e388
muwfs <wfongsak@hotmail.com>
2024-01-27 19:38:29 -0800

    Edit test script add test cases

--- IntroToUnitTesting/src/test/java/DemoTest.java
+++ IntroToUnitTesting/src/test/java/DemoTest.java
@@ -12,27 +12,27 @@ import org.junit.Test;
 
 public class DemoTest {
 	@Test
-	public void test_is_triangle_1() {
+	public void test_is_regularTriangle() {
 		assertTrue(Demo.isTriangle(3, 4, 5));
 	}
 	
 	@Test
-	public void test_is_triangle_2() {
+	public void test_is_regularTriangle_2() {
 		assertTrue(Demo.isTriangle(5, 12, 13));
 	}
 	
 	@Test
-	public void test_is_triangle_3() {
+	public void test_is_regularTriangle_3() {
 		assertTrue(Demo.isTriangle(5, 13, 12));
 	}
 	
 	@Test
-	public void test_is_triangle_4() {
-		assertTrue(Demo.isTriangle(12, 5, 13));
+	public void test_is_regularTriangle_4() {
+		assertTrue(Demo.isTriangle(8, 11, 12));
 	}
 	@Test
-    public void test_is_triangle_5() {
-        assertTrue(Demo.isTriangle(12, 13, 5));
+    public void test_is_sumOfTwoSidesIsLessThanThirdSide() {
+        assertFalse(Demo.isTriangle(1, 3, 4));
     }
 	
 	@Test
@@ -41,67 +41,68 @@ public class DemoTest {
 	}
 	
 	@Test
-	public void test_is_NOT_triangle_2() {
-		assertFalse(Demo.isTriangle(5, 13, 7));
+	public void test_is_NOT_triangle_aMinusBisC() {
+		assertFalse(Demo.isTriangle(5, 1, 4));
 	}
 	
 	@Test
-	public void test_is_NOT_triangle_3() {
-		assertFalse(Demo.isTriangle(13, 5, 7));
+	public void test_is_NOT_triangle_checkOneZero() {
+		assertFalse(Demo.isTriangle(2, 0, 4));
 	}
 	
 	@Test
-	public void test_is_NOT_triangle_4() {
-		assertFalse(Demo.isTriangle(13, 7, 5));
+	public void test_is_NOT_triangle_checkTwoZeroes() {
+		assertFalse(Demo.isTriangle(0, 0, 4));
 	}
 	
-	@Test
-	public void test_is_NOT_triangle_5() {
-		assertFalse(Demo.isTriangle(13, 7, 5));
-	}
 	
 	@Test
-	public void test_is_NOT_triangle_6() {
-		//This is NOT a triangle... and yet...
-		assertFalse(Demo.isTriangle(5, 9, 3));
-	}
-	
-	@Test
-	public void test_is_NOT_triangle_7() {
+	public void test_is_NOT_triangle_checkNegativeNumber() {
 		//Just no ...
-		assertFalse(Demo.isTriangle(1, 2, -1));
+		assertFalse(Demo.isTriangle(-1, 3, 4));
 	}
 	
 	@Test
-	public void test_is_NOT_triangle_8() {
+	public void test_is_NOT_triangle_checkNegativeNumber2() {
+		//Just no ...
+		assertFalse(Demo.isTriangle(3, -3, 0));
+	}
+	
+	@Test
+	public void test_is_NOT_triangle_checkNegativeNumber3() {
+		//Just no ...
+		assertFalse(Demo.isTriangle(+3, +3, -6));
+	}
+	
+	@Test
+	public void test_is_NOT_triangle_sumEqualsLength() {
 		//Just no ...
 		assertFalse(Demo.isTriangle(1, 2, 3));
 	}
 	
 	@Test
-	public void test_is_NOT_triangle_9() {
+	public void test_is_NOT_triangle_sumEqualsLength2() {
 		//Just no ...
-		assertFalse(Demo.isTriangle(2, 1, 3));
+		assertFalse(Demo.isTriangle(1, 1, 2));
 	}
 	
 	@Test
-	public void test_is_NOT_triangle_10() {
+	public void test_is_NOT_triangle_testRightTriangle() {
 		//Just no ...
-		assertFalse(Demo.isTriangle(3, 2, 1));
-	}
-	
-	@Test
-	public void test_is_NOT_triangle_11() {
-		//Just no ...
-		assertFalse(Demo.isTriangle(0, 0, 0));
+		assertFalse(Demo.isTriangle(3, 3, 3));
 	}
 	@Test
-	public void test_is_NOT_triangle_12() {
+	public void test_is_NOT_triangle_checkPlus() {
 		//Just no ...
-		assertFalse(Demo.isTriangle(12.3, 0, -3));
+		assertFalse(Demo.isTriangle(+5, +4, +3));
 	}
 	
-	/*
+    @Test
+    public void test_is_NOT_triangle_checkEmpty(){
+        assertFalse(Demo.isTriangle('\n',3,'!'));
+    }
+	
+    
 	@Test
 	public void test_main_program_1() {
 		String input = "5\n12\n13\n";
@@ -121,7 +122,6 @@ public class DemoTest {
 		
 		assertEquals(consoleOutput, out.toString());	
 	}
-	*/
 	
 	
 }
